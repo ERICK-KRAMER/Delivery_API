3d75ddc126f05ccbfc5bed585bfb107bfd220182
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-07-31 17:57:47 -0300

    feat: create new entity, product

---
+++ entities/Product.cs
@@ -0,0 +1,19 @@
+namespace Delivery.Product
+{
+    public class Product
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public Product(string name, string description, decimal price, decimal discountPercent)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+            DiscountPercent = discountPercent;
+        }
+    }
+}
