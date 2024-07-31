adce5ff6b3b4390f1b543948a554987fba074555
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-07-31 17:52:26 -0300

    feat: create entity user

---
+++ entities/User.cs
@@ -0,0 +1,18 @@
+namespace Delivery.User
+{
+    public class User
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string name, string email, string password)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
