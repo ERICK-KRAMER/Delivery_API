91d16a101467d39004531492348a0142f34d79d9
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-08-02 00:59:09 -0300

    feat: implement interface order in order repository

--- Repositories/interfaces/IOrderRepositopry.cs
+++ Repositories/interfaces/IOrderRepositopry.cs
@@ -5,9 +5,8 @@ namespace Delivery.Repository.Interface
     public interface IOrderRepository
     {
         Task<List<Order>> GetAllOrder();
-        Task<Order> GetOrderById(Guid id);
+        Task<Order?> GetOrderById(Guid id);
         Task<Order> CreateOrder(Order order);
-        Task<Order> UpdateOrder(Order order);
         Task DeleteOrder(Guid id);
     }
 }
